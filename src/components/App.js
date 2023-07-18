302a021aeeabc1af81b7fe517a0161ee156bc99d
dhanraj-saur <dhananjayverma301@gmail.com>
2023-07-18 23:48:12 +0530

    ho na ja bro

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,20 @@ import Tabs from './Tabs';
 
 const App = () => {
   const tabs1 = [
-    { title: 'Tab 1', content: 'Content 1' },
-    { title: 'Tab 2', content: 'Content 2' },
-    { title: 'Tab 3', content: 'Content 3' },
+    { title: 'Tab 1', content: 'Content for Tab 1' },
+    { title: 'Tab 2', content: 'Content for Tab 2' },
+    { title: 'Tab 3', content: 'Content for Tab 3' },
   ];
 
   const tabs2 = [
-    { title: 'Tab A', content: 'Content A' },
-    { title: 'Tab B', content: 'Content B' },
-    { title: 'Tab C', content: 'Content C' },
+    { title: 'Tab A', content: 'Content for Tab A' },
+    { title: 'Tab B', content: 'Content for Tab B' },
+    { title: 'Tab C', content: 'Content for Tab C' },
   ];
 
   return (
     <div>
+      
       <Tabs tabs={tabs1} key="tabs1" />
       <Tabs tabs={tabs2} key="tabs2" />
     </div>
